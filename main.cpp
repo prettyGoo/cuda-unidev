e18cb31cf15f9137b38b28d869f2feea28410ee1
Nikita Kirilov <kirilov.dev@gmail.com>
2017-10-05 00:16:05 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+// CUDA_1_PC.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+const unsigned int arrsize = 1000000;
+
+void initArr(double* arr);
+void sumArr(double* firstArr, double* secondArr);
+
+int main()
+{
+	srand(time(0));
+
+	unsigned int start_time = clock(); // íà÷àëüíîå âðåìÿ
+
+	double *a = new double[arrsize];
+	double *b = new double[arrsize];
+
+	initArr(a);
+	initArr(b);
+
+	sumArr(a, b);
+
+	unsigned int end_time = clock(); // êîíå÷íîå âðåìÿ
+
+	cout << "Time: " << end_time - start_time << endl;
+
+	system("pause");
+
+    return 0;
+}
+
+void initArr(double* arr) {
+	for (int i = 0; i < arrsize; i++)
+		arr[i] = rand() % 100 + 1;
+	cout << "OK" << endl;
+}
+
+void sumArr(double* firstArr, double* secondArr) {
+	for (int i = 0; i < arrsize; i++)
+		firstArr[i] += secondArr[i];
+	cout << "DONE" << endl;
+}
