4733f10c7d715f6e2ebce051e7e97b3390ab0a15
Nikita Kirilov <kirilov.dev@gmail.com>
2017-10-05 00:27:00 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,48 +1,49 @@
-// CUDA_1_PC.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
 #include <iostream>
 #include <ctime>
+#include <cstdio>
 
 using namespace std;
 
-const unsigned int arrsize = 1000000;
+const unsigned int arrsize = 100000000;
 
-void initArr(double* arr);
-void sumArr(double* firstArr, double* secondArr);
+void initArr(float* arr);
+void sumArr(float* firstArr, float* secondArr, float* finalArr);
 
 int main()
 {
 	srand(time(0));
 
-	unsigned int start_time = clock(); // íà÷àëüíîå âðåìÿ
+	unsigned int start_time = clock();
 
-	double *a = new double[arrsize];
-	double *b = new double[arrsize];
+	float *a = new float[arrsize];
+	float *b = new float[arrsize];
+	float *c = new float[arrsize];
 
 	initArr(a);
 	initArr(b);
 
-	sumArr(a, b);
+	sumArr(a, b, c);
 
-	unsigned int end_time = clock(); // êîíå÷íîå âðåìÿ
+	unsigned int end_time = clock();
 
 	cout << "Time: " << end_time - start_time << endl;
 
-	system("pause");
+	delete a;
+	delete b;
+	delete c;
 
-    return 0;
+	system("pause");
+	return 0;
 }
 
-void initArr(double* arr) {
+void initArr(float* arr) {
 	for (int i = 0; i < arrsize; i++)
 		arr[i] = rand() % 100 + 1;
 	cout << "OK" << endl;
 }
 
-void sumArr(double* firstArr, double* secondArr) {
+void sumArr(float* firstArr, float* secondArr, float* finalArr) {
 	for (int i = 0; i < arrsize; i++)
-		firstArr[i] += secondArr[i];
+		finalArr[i] = firstArr[i] + secondArr[i];
 	cout << "DONE" << endl;
 }
